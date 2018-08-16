7f1a4f9be47e95161724c06900da369d63c385dc
Zander Tulloch <ztulloch@gmail.com>
2018-08-16 14:09:38 +0100

    Added an extra info field. Added an override flag for testing.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-#![feature(assoc_unix_epoch)]
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::fs;
+use std::io;
 use std::fs::OpenOptions;
 use std::error::Error;
 use std::path::Path;
@@ -22,6 +22,7 @@ struct Unit {
     start_time: u64,
     end_time: u64,
     duration: u64,
+    task: String,
 }
 
 // Struct for a running timer
@@ -29,6 +30,7 @@ struct Unit {
 struct Timer {
     start_time: u64,
     project_code: String,
+    task: String,
 }
 
 // function that checks if a file exists
@@ -76,6 +78,7 @@ fn print_status () -> Result<(), Box<Error>>  {
     Ok(())
 }
 
+// Print program usage
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} start/stop/cancel/status/hours/weeks [options]", program);
     print!("{}", opts.usage(&brief));
@@ -83,7 +86,7 @@ fn print_usage(program: &str, opts: Options) {
 
 // initial print hours function just to test reading in log file
 // Just reads the log csv file and prints it out
-fn print_hours() -> Result<(), Box<Error>> {
+fn print_hours(quash: bool) -> Result<(), Box<Error>> {
     if file_exists("logger.csv") {
         // read start timer struct from timer file
         let mut counter=0;
@@ -94,14 +97,14 @@ fn print_hours() -> Result<(), Box<Error>> {
             .open("logger.csv")
             .unwrap();
         let mut rdr = csv::ReaderBuilder::new()
-            .has_headers(false)
+            .has_headers(true)
             .from_reader(file);
         for result in rdr.deserialize() {
             let unit: Unit = result?;
-            if unit.duration>120 { // ignore anything less than 2 minutes
+            if unit.duration>120 || quash { // ignore anything less than 2 minutes
                 let naive_datetime = NaiveDateTime::from_timestamp(unit.start_time as i64, 0);
                 let datetime_again: DateTime<Utc> = DateTime::from_utc(naive_datetime, Utc);
-                println!("Project: {}       date {:?} duration {} hrs {} mins", unit.project_code, datetime_again, unit.duration/60/60, unit.duration/60%60);
+                println!("Project: {} Task: {}      date {:?} duration {} hrs {} mins", unit.project_code, unit.task, datetime_again, unit.duration/60/60, unit.duration/60%60);
                 counter+=unit.duration;
             }
         };
@@ -117,7 +120,7 @@ fn print_hours() -> Result<(), Box<Error>> {
 
 // print hours tracked in terms of weeks
 // need to extend to print on project basis
-fn print_weeks() -> Result<(), Box<Error>> {
+fn print_weeks(quash: bool) -> Result<(), Box<Error>> {
     if file_exists("logger.csv") {
         // read start timer struct from timer file
         let mut counter=0;
@@ -129,11 +132,11 @@ fn print_weeks() -> Result<(), Box<Error>> {
             .open("logger.csv")
             .unwrap();
         let mut rdr = csv::ReaderBuilder::new()
-            .has_headers(false)
+            .has_headers(true)
             .from_reader(file);
         for result in rdr.deserialize() {
             let unit: Unit = result?;
-            if unit.duration>120 { // ignore anything less than 2 minutes
+            if unit.duration>120 || quash { // ignore anything less than 2 minutes
                 let naive_datetime = NaiveDateTime::from_timestamp(unit.start_time as i64, 0);
                 if week_pointer==naive_datetime.iso_week().week() {
                     counter+=unit.duration;
@@ -157,7 +160,7 @@ fn print_weeks() -> Result<(), Box<Error>> {
 }
 
 // start timer - just writes the current time in seconds to a csv file
-fn start_timer (project_code: &str) {
+fn start_timer (project_code: &str, task: &str) {
     // don't start a new timer if one is running
     if file_exists("timer.csv") {
         println!("Timer is already running");
@@ -178,6 +181,7 @@ fn start_timer (project_code: &str) {
         wtr.serialize(Timer {
             start_time: start_time.as_secs(),
             project_code: project_code.to_string(),
+            task: task.to_string(),
         }).expect ("Error creating timer file");
         
         wtr.flush().expect("Error creating timer");
@@ -188,7 +192,7 @@ fn start_timer (project_code: &str) {
 }
 
 // reads timer file and writes results to log file
-fn stop_timer() -> Result<(), Box<Error>> {
+fn stop_timer(quash: bool) -> Result<(), Box<Error>> {
     if file_exists("timer.csv") {
         // read start timer struct from timer file
         let file = OpenOptions::new()
@@ -210,9 +214,10 @@ fn stop_timer() -> Result<(), Box<Error>> {
 
             // calculate time difference
             let time_difference=stop_time.as_secs()-timer.start_time;
-            if time_difference>120 {
+            if time_difference>120 || quash {
                 println!("Timer has been running {} hrs {} mins", time_difference/60/60, time_difference/60%60);
-
+                // use headers if it's the first time writing the logger file
+                let existence = file_exists("logger.csv");
                 // take timer data and add it to the log file along with the current time and the delta
                 let mut file = OpenOptions::new()
                     .write(true)
@@ -220,7 +225,9 @@ fn stop_timer() -> Result<(), Box<Error>> {
                     .append(true)
                     .open("logger.csv")
                     .unwrap();
-                let mut wtr = csv::WriterBuilder::new()
+                if existence {
+                    println!("Building write without any headers");
+                    let mut wtr = csv::WriterBuilder::new()
                     .has_headers(false)
                     .from_writer(file);
                 
@@ -229,9 +236,27 @@ fn stop_timer() -> Result<(), Box<Error>> {
                     start_time: timer.start_time,
                     end_time: stop_time.as_secs(),
                     duration: time_difference,
+                    task: timer.task,
                 }).expect("Error writing logfile");
                 
-                wtr.flush().expect("Error writing logfile");
+                    wtr.flush().expect("Error writing logfile");
+                } else {
+                    println!("Building writer with headers");
+                let mut wtr = csv::WriterBuilder::new()
+                    .has_headers(true)
+                    .from_writer(file);
+                
+                wtr.serialize(Unit {
+                    project_code: timer.project_code,
+                    start_time: timer.start_time,
+                    end_time: stop_time.as_secs(),
+                    duration: time_difference,
+                    task: timer.task,
+                }).expect("Error writing logfile");
+                
+                    wtr.flush().expect("Error writing logfile");
+
+                }
             } else {
                 println!("Timer has been running for less than 2 minutes, discarding...");
             }
@@ -263,13 +288,18 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
     let mut project_code="default".to_string();
+    let mut task=String::new();//"default".to_string();
     let mut working_directory="default".to_string();
-
+    let mut quash = false; // override 120 second limit on timers
     let mut opts = Options::new();
     // -p PROJECT - project flag
     opts.optopt("p", "", "set user definable project code.", "CODE");
     // -d PROJECT - project directory
     opts.optopt("d", "", "set working directory. Overrides $TIMERDIR environment variable.", "CODE");
+    // -t TASK - task 
+    opts.optopt("t", "", "set project task. Short summary of task undertaken.", "CODE");
+    // -q  - quash
+    opts.optflag("q", "quash", "Overrides 2 minute limit. Useful for testing.");
     // -h help - print usage
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
@@ -295,7 +325,18 @@ fn main() {
             None => println!("No working directory specified"),
         }
     }
-
+    // t process task
+    if matches.opt_present("t") {
+        match matches.opt_str("t") {
+            Some(x) => task=x,
+            None => println!("No task specififed, using default"),
+        }
+    }
+    // o override
+    if matches.opt_present("q") {
+        quash=true;
+    }
+    
     let command = if !matches.free.is_empty() {
         matches.free[0].clone()
     } else {
@@ -323,18 +364,54 @@ fn main() {
     };
     
     if command=="start" {
-        start_timer (&project_code);
+        // check task status and request if not available
+        let mut input = String::new();
+        let mut done = false;
+        while !done {
+            if task != "" { break; }
+            println!("No task specified. What will you be working on?");
+            println!("Please keep it brief and don't punctuate.");
+            io::stdin().read_line(&mut input)
+                .expect("Failed to read line");
+            task=input.trim().to_string();
+            if task.contains(",") { 
+                println!("Input string contains illegal character {}", task);
+                task.drain(..);
+                input.drain(..);
+            } else {
+                done = true;
+            }
+        }
+        // check project code. If unset request from user
+        done = false;
+        input.drain(..);
+        while !done {
+            if project_code != "default" { break; }
+            println!("No project specified. Please specify a one word project.");
+            io::stdin().read_line(&mut input)
+                .expect("Failed to read line");
+            project_code=input.trim().to_string();
+            if project_code.contains(",") { 
+                println!("Input string contains illegal character {}", project_code);
+                project_code.drain(..);
+                input.drain(..);
+            } else {
+                done = true;
+            }
+        }
+        
+        start_timer (&project_code, &task);
     } else if command=="stop" {
         println! ("Stopping timer...");
-        stop_timer ().expect("Error stopping timer");
+        stop_timer (quash).expect("Error stopping timer");
     } else if command=="cancel" {
         cancel_timer ().expect("Error cancelling timer");
     } else if command=="status" {
         print_status ().expect("Unable to parse log file");
     } else if command=="hours" {
-        print_hours().expect("Unable to parse log file");
+        print_hours(quash).expect("Unable to parse log file");
     } else if command=="weeks" {
-        print_weeks().expect("Unable to parse log file");
+        print_weeks(quash).expect("Unable to parse log file");
     } else {
         println!("Unknown option `{}`.", command);
         println!("");
